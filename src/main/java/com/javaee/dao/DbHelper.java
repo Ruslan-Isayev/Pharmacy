780b11f60d73ac03b16f73d62a87c23813866766
Ruslan Isayev <blvck888@mail.ru>
2023-03-31 11:36:21 +0400

    new commit

---
+++ src/main/java/com/javaee/dao/DbHelper.java
@@ -0,0 +1,14 @@
+package com.javaee.dao;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+public class DbHelper {
+    public static Connection getConnection() throws Exception{
+        Context context = new InitialContext();
+        DataSource dataSource = (DataSource) context.lookup("java:comp/env/jdbc/ePharmacy");
+        Connection c = dataSource.getConnection();
+        return c;
+    }
+}
